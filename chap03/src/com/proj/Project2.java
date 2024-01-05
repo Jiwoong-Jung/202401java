d990bfbe1175e26f2e8a44c2dd0bda54568adc58
Jiwoong-Jung <jjw981119@naver.com>
2024-01-05 11:59:46 +0900

    Project2 완성

--- chap03/src/com/proj/Project2.java
+++ chap03/src/com/proj/Project2.java
@@ -2,38 +2,15 @@ package com.proj;
 
 import java.util.Scanner;
 
-public class Project1 {
-	static boolean loginStatus;
-	public static void login(Scanner scanner, 
-										String memberName, String memberSSN ) {
-//		if (loginStatus) {  // 로그인 여부 확인
-//			System.out.println("이미 로그인 상태입니다.");
-//			continue;
-//		}
-		
-		System.out.print("아이디:");  //출력문
-		String name = scanner.nextLine(); // name 변수의 값 입력
-		System.out.print("패스워드:");
-		String strPassword = scanner.nextLine();//패스워드입력
-		//int password = Integer.parseInt(strPassword); // stpassword값을 정수로 변환
-		if (name.equals(memberName)) {
-			if (strPassword.equals(memberSSN)) {
-				loginStatus = true;
-				System.out.println("로그인 성공");
-			} else {
-				System.out.println("로그인 실패:패스워드가 틀림");
-			}
-		} else {
-			System.out.println("로그인 실패:아이디 존재하지 않음");
-		}
-	}
+public class Project2 {
+
 	public static void main(String[] args) {
-		String memberName = "admin";  // 회원 이름
-		String memberSSN = "1111";   // 회원 생년월일 (패스워드로 사용)
-		String memberTel = "010-1234-1234";   // 회원 전화번호
+		String memberName = "";
+		String memberSSN = "";
+		String memberTel = "";
+		int balance = 0;
 		boolean loginStatus = false; // 로그인 여부
 		boolean membershipStatus = false; // 회원가입 여부
-		int balance = 0; // 은행 잔고
 		Scanner scanner = new Scanner(System.in);
 		boolean run = true;
 		boolean run2 = false;
@@ -45,16 +22,19 @@ public class Project1 {
 			int menuNum = Integer.parseInt(scanner.nextLine());
 			switch (menuNum) {
 			case 1:
+				if (!membershipStatus) {
+					System.out.println("회원 가입을 하시기 바랍니다.");
+					continue;
+				}
 				if (loginStatus) {  // 로그인 여부 확인
 					System.out.println("이미 로그인 상태입니다.");
 					continue;
 				}
-				
 				System.out.print("아이디:");  //출력문
 				String name = scanner.nextLine(); // name 변수의 값 입력
 				System.out.print("패스워드:");
 				String strPassword = scanner.nextLine();//패스워드입력
-				//int password = Integer.parseInt(strPassword); // stpassword값을 정수로 변환
+//				int password = Integer.parseInt(strPassword); // stpassword값을 정수로 변환
 				if (name.equals(memberName)) {
 					if (strPassword.equals(memberSSN)) {
 						loginStatus = true;
@@ -74,7 +54,7 @@ public class Project1 {
 						System.out.print("가입절차를 계속 진행 하시겠습니까?(y/n) ");
 						String answer = scanner.nextLine();
 						if (answer.equals("n") || answer.equals("N")) {
-//							run = true;
+							run = true;
 							continue AAA;
 						} else if (answer.equals("y") || answer.equals("Y")) {
 							break;
@@ -102,7 +82,6 @@ public class Project1 {
 				} else {
 					System.out.println("로그인 하시기 바랍니다.");
 				}
-				
 				while (run2) {
 					System.out.println("-------------------------------------");
 					System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
@@ -128,14 +107,12 @@ public class Project1 {
 					}
 					System.out.println();
 				}
-				System.out.println("프로그램 종료");
 				break;
 			case 4:
 				run = false;
 				break;
 			}
 		}
-		scanner.close();
 		System.out.println("프로그램 종료");
 	}
 }
