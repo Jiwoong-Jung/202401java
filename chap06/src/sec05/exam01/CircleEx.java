e246874f350e8b743802df7df299129f66c4bc62
Jiwoong-Jung <jjw981119@naver.com>
2024-01-10 14:47:01 +0900

    Circle() 수정

--- chap06/src/sec05/exam01/CircleEx.java
+++ chap06/src/sec05/exam01/CircleEx.java
@@ -5,8 +5,13 @@ public class CircleEx {
 	public static void main(String[] args) {
 		Circle[] c;
 		c = new Circle[5];
+		
 		for (int i=0; i < c.length; i++) {
 			c[i] = new Circle(i);
+		}
+		
+		for (int i=0; i < c.length; i++) {
+			System.out.println(c[i].getArea());
 		}
 
 	}
