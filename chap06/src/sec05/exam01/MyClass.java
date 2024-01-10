c46b881c646918b8b0a561a462850fb979ed55db
Jiwoong-Jung <jjw981119@naver.com>
2024-01-10 14:29:33 +0900

    this() 추가2

--- chap06/src/sec05/exam01/MyClass.java
+++ chap06/src/sec05/exam01/MyClass.java
@@ -22,13 +22,20 @@ public class MyClass {
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		System.out.println("특별 명령");
 		System.out.println("3번째 생성자");
+	}
+
+	@Override
+	public String toString() {
+		return "MyClass [x=" + x + ", y=" + y + ", z=" + z + "]";
 	}
 
 	public static void main(String[] args) {
 		MyClass mc = new MyClass(100);
-		
-
+		System.out.println(mc);
+		MyClass mc2 = new MyClass(100, 200);
+		System.out.println(mc2);
 	}
 
 }
