783ad3c6de4228585213b46c7d499171d6533d86
Jiwoong-Jung <jjw981119@naver.com>
2024-01-12 14:17:41 +0900

    2024-01-12 Project1.java 수정

--- java0112/src/com/project/Project1.java
+++ java0112/src/com/project/Project1.java
@@ -10,6 +10,38 @@ public class Project1 {
 		System.out.println("-------------------------------------");
 	}
 	
+	public static void disp2() {
+		System.out.println("-------------------------------------");
+		System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.상위메뉴");
+		System.out.println("-------------------------------------");
+	}
+	
+	public static void menu2(Session session) {
+		while (session.isRun2()) {
+			disp2();
+			System.out.print("선택> ");
+			int menuNum = Integer.parseInt(scanner.nextLine());
+			switch (menuNum) {
+			case 1:
+				// 로그인 처리
+				System.out.println("로그인 처리");
+				break;
+			case 2:
+				// 회원 가입
+				System.out.println("회원 가입");
+				break;
+			case 3:
+				// 예금 출금
+				System.out.println("예금 출금");
+				break;
+			case 4:
+				session.setRun2(false);
+				System.out.println("프로그램 종료");
+				break;
+			}
+		}
+	}
+	
 	public static void menu1(Session session) {
 		while (session.isRun()) {
 			disp1();
@@ -27,13 +59,14 @@ public class Project1 {
 			case 3:
 				// 예금 출금
 				System.out.println("예금 출금");
+				menu2(session);
 				break;
 			case 4:
 				session.setRun(false);
+				System.out.println("프로그램 종료");
 				break;
 			}
 		}
-		System.out.println("프로그램 종료");
 	}
 	public static void main(String[] args) {
 		Session session = new Session(true, true);
