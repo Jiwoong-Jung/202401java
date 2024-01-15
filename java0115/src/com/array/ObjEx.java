e68ff33543e0ca966579d50a3a54cc59acdebafb
Jiwoong-Jung <jjw981119@naver.com>
2024-01-15 16:29:11 +0900

    2024-01-15 객체 파일 처리

--- java0115/src/com/array/ObjEx.java
+++ java0115/src/com/array/ObjEx.java
@@ -1,6 +1,13 @@
 package com.array;
 
-class Acc {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+class Acc implements Serializable {
 	int x;
 	String y;
 	
@@ -11,11 +18,29 @@ class Acc {
 }
 public class ObjEx {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		Acc acc = new Acc();
 		acc.x = 100;
 		acc.y = "가나다";
 		System.out.println(acc);
+		FileOutputStream fos = new FileOutputStream("c:/temp/acc.db");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(acc);
+		oos.flush();
+		oos.close();
+		
+		FileInputStream fis = new FileInputStream("c:/temp/acc.db");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		Acc acc2 = (Acc) ois.readObject();
+		System.out.println(acc2);
 	}
 
 }
+
+
+
+
+
+
+
+
