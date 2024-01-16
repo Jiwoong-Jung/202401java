e723fb30e09bd291a5537a40e2dccf62a4e2b860
Jiwoong-Jung <jjw981119@naver.com>
2024-01-16 15:12:34 +0900

    2024-01-16 오후

--- java0116/src/sec02/exam01/PromoEx.java
+++ java0116/src/sec02/exam01/PromoEx.java
@@ -9,10 +9,20 @@ class E extends C {}
 
 public class PromoEx {
 	public static void main(String[] args) {
+		Object obj = new A();
 		B b = new B();
 		C c = new C();
 		D d = new D();
-		E e = new E(); 
+		E e = new E();
+//		int y = 100;
+//		double x = y;
+		A a1 = b;
+		A a2 = c;
+		A a3 = d;
+		A a4 = e;
+		
+		B b1 = d;
+		C c1 = e; // 부모로 변수선언, 자식으로 객체생성
 	}
 
 }
