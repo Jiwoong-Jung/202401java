fe186cc41e0024c1ca74f238f619e67d6f15eaa3
Jiwoong-Jung <jjw981119@naver.com>
2024-01-16 14:10:23 +0900

    2024-01-16 오전

---
+++ java0116/src/java0116/Bb.java
@@ -0,0 +1,22 @@
+package java0116;
+
+class Aa {
+	private int y;
+	final int x;
+	Aa() {
+		this.x = 0;
+	}
+	Aa(int x) {
+		this.x = x;
+	}
+	Aa(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+public class Bb extends Aa {
+	void method() {
+//		x = 40; final 필드 값 변경 안됨
+//		System.out.println(y); private 접근 안됨
+	}
+}
