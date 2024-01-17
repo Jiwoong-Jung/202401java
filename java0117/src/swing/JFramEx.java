fe4d230baec51eb34356397e65d30711d280cd1f
Jiwoong-Jung <jjw981119@naver.com>
2024-01-17 15:08:23 +0900

    2024-01-17 자바 스윙

---
+++ java0117/src/swing/JFramEx.java
@@ -0,0 +1,14 @@
+package swing;
+
+import javax.swing.JFrame;
+
+public class JFramEx {
+
+	public static void main(String[] args) {
+		JFrame jf = new JFrame();
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setSize(300, 300);
+		jf.setVisible(true);
+	}
+
+}
