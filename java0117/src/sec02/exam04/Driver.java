34a1b57b299f158f59c8200492aa05d01ed7f9c9
Jiwoong-Jung <jjw981119@naver.com>
2024-01-17 10:21:41 +0900

    2024-01-17 매개 변수의 다형성

---
+++ java0117/src/sec02/exam04/Driver.java
@@ -0,0 +1,7 @@
+package sec02.exam04;
+
+public class Driver {
+	public void drive(Vehicle vehicle) {
+		vehicle.run();
+	}
+}
---
+++ java0117/src/sec02/exam04/Vehicle.java
@@ -0,0 +1,7 @@
+package sec02.exam04;
+
+public class Vehicle {
+	public void run() {
+		System.out.println("차량이 달린다.");
+	}
+}
