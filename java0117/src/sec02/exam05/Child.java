962f5217de7c534a3ca045016b221915c244f12f
Jiwoong-Jung <jjw981119@naver.com>
2024-01-17 10:52:42 +0900

    2024-01-17 강제 타입 변환

---
+++ java0117/src/sec02/exam05/Child.java
@@ -0,0 +1,9 @@
+package sec02.exam05;
+
+public class Child extends Parent {
+	public String field2;
+	
+	public void method3() {
+		System.out.println("Child-method3()");
+	}
+}
---
+++ java0117/src/sec02/exam05/Parent.java
@@ -0,0 +1,12 @@
+package sec02.exam05;
+
+public class Parent {
+	public String field1;
+	
+	public void method1() {
+		System.out.println("Parent-method1()");
+	}
+	public void method2() {
+		System.out.println("Parent-method2()");
+	}
+}
