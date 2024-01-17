2a24f2e577ab8ec667267a26c95860ebe63de2a8
Jiwoong-Jung <jjw981119@naver.com>
2024-01-17 15:26:53 +0900

    2024-01-17 스윙

--- java0117/src/swing/MyFrame.java
+++ java0117/src/swing/MyFrame.java
@@ -1,12 +1,21 @@
 package swing;
 
+import java.awt.Container;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame {
 	public MyFrame() {
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(300, 300);  // 크기 설정
+		Container con = this.getContentPane();
+		JButton jb1 = new JButton("버튼");
+		con.add(jb1);
+		
+		this.setLocation(200, 100);
+		this.setTitle("스윙 연습");
 		this.setVisible(true);  // 윈도우 프레임을 보이게
+		this.setSize(500, 300);  // 크기 설정
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	public static void main(String... ar) {
