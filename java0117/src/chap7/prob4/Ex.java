bbf70f55a83cbfe2d7a83b720ec0b1e75068ee85
Jiwoong-Jung <jjw981119@naver.com>
2024-01-17 14:15:44 +0900

    2024-01-17 확인문제4

--- java0117/src/chap7/prob4/Ex.java
+++ java0117/src/chap7/prob4/Ex.java
@@ -4,7 +4,7 @@ public class Ex {
 
 	public static void main(String[] args) {
 		Controller controller = new Controller();
-		controller.setService(null);
+		controller.setService(new BService());
 
 	}
 
