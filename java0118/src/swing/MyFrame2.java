b60873cdc8cd4852acb0ca33f5b77d7ed3a10198
Jiwoong-Jung <jjw981119@naver.com>
2024-01-18 15:43:46 +0900

    2024-01-18 자바 스윙 이벤트

---
+++ java0118/src/swing/MyFrame2.java
@@ -0,0 +1,40 @@
+package swing;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MyFrame2 extends JFrame implements ActionListener{
+	JButton jb = new JButton("버튼1");
+	JButton jb2 = new JButton("버튼2");
+	public MyFrame2() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container con = this.getContentPane();  // 컴포넌트 붙이는 곳
+		this.setVisible(true);
+		this.setSize(500, 300);
+		this.setLocation(300, 300);
+		this.setTitle("오늘은 이벤트");
+		
+		con.setLayout(new FlowLayout());
+		
+		con.add(jb);
+		jb.addActionListener(this);
+		
+		con.add(jb2);
+		jb2.addActionListener(this);
+	}
+	
+	public static void main(String[] ar) {
+		new MyFrame2();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println(e.getSource()==jb);
+		
+	}
+}
