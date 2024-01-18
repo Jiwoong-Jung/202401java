01df8dc51bc09181b60cf778e2676ef2c6958a14
Jiwoong-Jung <jjw981119@naver.com>
2024-01-18 14:42:10 +0900

    2024-01-18 인터페이스

---
+++ java0118/src/sec01/exam02/MyClass4.java
@@ -0,0 +1,17 @@
+package sec01.exam02;
+
+public class MyClass4 implements InterTest2{
+
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void test6() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
