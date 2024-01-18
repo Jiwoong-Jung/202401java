9019740ed92de1d2917ecb3a520dcd5d042b4121
Jiwoong-Jung <jjw981119@naver.com>
2024-01-18 14:55:02 +0900

    2024-01-18 자바 스윙

--- java0118/src/swing/MyFrame.java
+++ java0118/src/swing/MyFrame.java
@@ -1,10 +1,13 @@
 package swing;
 
+import java.awt.Container;
+
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame {
 	public MyFrame() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container con = this.getContentPane();  // 컴포넌트 붙이는 곳
 		this.setVisible(true);
 		this.setSize(500, 300);
 		this.setLocation(300, 300);
