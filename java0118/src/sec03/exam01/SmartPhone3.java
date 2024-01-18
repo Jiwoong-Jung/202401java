a540992edc5963d36b8203c0dc58994e47e5fefe
Jiwoong-Jung <jjw981119@naver.com>
2024-01-18 10:00:17 +0900

    2024-01-18 오전

---
+++ java0118/src/sec03/exam01/SmartPhone3.java
@@ -0,0 +1,22 @@
+package sec03.exam01;
+
+public class SmartPhone3 extends SmartPhone {
+
+	public SmartPhone3(int x) {
+		super(x);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String ring(int tel) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
