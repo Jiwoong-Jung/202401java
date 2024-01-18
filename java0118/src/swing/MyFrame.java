db5f78b8e8ef5243ab35369d2c9f1b8b786a0cbb
Jiwoong-Jung <jjw981119@naver.com>
2024-01-18 14:54:46 +0900

    2024-01-18 자바 스윙

--- java0118/src/swing/MyFrame.java
+++ java0118/src/swing/MyFrame.java
@@ -6,6 +6,8 @@ public class MyFrame extends JFrame {
 	public MyFrame() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
+		this.setSize(500, 300);
+		this.setLocation(300, 300);
 	}
 	
 	public static void main(String[] ar) {
