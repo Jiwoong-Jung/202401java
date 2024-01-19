7e894fbfd58aa8be4ea0b3a983950ca5499a2508
Jiwoong-Jung <jjw981119@naver.com>
2024-01-19 14:15:18 +0900

    2024-01-19 프록시 패턴

--- java0119/src/proxy/Client.java
+++ java0119/src/proxy/Client.java
@@ -3,6 +3,9 @@ package proxy;
 public class Client {
 
 	public static void main(String[] args) {
+//		ISubject sub1 = new RealSubject();
+//		sub1.action();
+		
 		ISubject sub = new Proxy(new RealSubject());
 		sub.action();
 
