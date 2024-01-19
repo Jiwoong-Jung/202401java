b61687b9b73c713da36159ed19f817788a5782ca
Jiwoong-Jung <jjw981119@naver.com>
2024-01-19 15:36:11 +0900

    2024-01-19 오후 자바 스윙

---
+++ java0119/src/swing/ver5/MyWin.java
@@ -0,0 +1,69 @@
+package swing.ver5;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class MyWin extends JFrame {
+	JLabel lb1 = new JLabel("이름1:");
+	JTextField tf1 = new JTextField(5);
+	JButton bt1 = new JButton("버튼1");
+	JLabel lb2 = new JLabel("이름2:");
+	JTextField tf2 = new JTextField(5);
+	JButton bt2 = new JButton("종료");
+
+	public MyWin() {
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		Container con = this.getContentPane();
+		con.setLayout(new FlowLayout());
+		con.add(lb1);
+		con.add(tf1);
+		con.add(bt1);
+		con.add(lb2);
+		con.add(tf2);
+		con.add(bt2);
+		this.setTitle("자바 스윙");
+		this.setSize(400, 300);
+		this.setLocation(700, 400);
+		this.setVisible(true);
+		bt1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("버튼1");
+
+			}
+		});
+		bt2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("종료합니다.");
+				System.exit(0); // 0: 정상종료
+
+			}
+		});
+	}
+
+	public void confirmExit() {
+		int answer = JOptionPane.showConfirmDialog(this, "종료하시겠습니까?", 
+				                         "confirm",JOptionPane.YES_NO_OPTION );
+		if(answer==JOptionPane.YES_OPTION){  //사용자가 yes를 눌렀을 경우
+			System.out.println("프로그램을 종료합니다.");
+			System.exit(0);
+		} else{  //사용자가 Yes 이외의 값을 눌렀을 경우
+			System.out.println("종료를 취소합니다.");
+		}
+	}
+	public static void main(String[] args) {
+		new MyWin();
+	}
+
+}
