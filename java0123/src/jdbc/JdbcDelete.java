4c6efbac8f0f3fe266596036f604daa655ae6ee0
Jiwoong-Jung <jjw981119@naver.com>
2024-01-23 16:51:18 +0900

    2024-01-23 오후 JDBC

--- java0123/src/jdbc/JdbcDelete.java
+++ java0123/src/jdbc/JdbcDelete.java
@@ -14,12 +14,12 @@ public class JdbcDelete {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection conn = DriverManager.getConnection(url,id,pass);
 		Statement stmt = conn.createStatement();
-		String sql = "insert into emp values (7777, '김하나', '사원', 7839, '2024-01-23', 3000, null, 10)";
+		String sql = "delete from emp where empno = 7777";
 		int result = stmt.executeUpdate(sql);
 		if (result >= 1) {
-			System.out.println("입력성공!"+result);
+			System.out.println("삭제성공!"+result);
 		} else {
-			System.out.println("입력실패!"+result);
+			System.out.println("삭제실패!"+result);
 		}
 
 	}
