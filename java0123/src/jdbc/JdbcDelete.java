3796b019afb4fa62fa94f2373c746d32fd35087f
Jiwoong-Jung <jjw981119@naver.com>
2024-01-23 16:46:19 +0900

    2024-01-23 오후 JDBC

---
+++ java0123/src/jdbc/JdbcDelete.java
@@ -0,0 +1,27 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcDelete {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		String url = "jdbc:mysql://localhost:3306/firm";
+		String id = "root";
+		String pass = "mysql";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection conn = DriverManager.getConnection(url,id,pass);
+		Statement stmt = conn.createStatement();
+		String sql = "insert into emp values (7777, '김하나', '사원', 7839, '2024-01-23', 3000, null, 10)";
+		int result = stmt.executeUpdate(sql);
+		if (result >= 1) {
+			System.out.println("입력성공!"+result);
+		} else {
+			System.out.println("입력실패!"+result);
+		}
+
+	}
+
+}
