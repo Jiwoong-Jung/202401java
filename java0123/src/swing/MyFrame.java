970044931120d3a9476984c14566fec428e1fc3c
Jiwoong-Jung <jjw981119@naver.com>
2024-01-23 10:35:09 +0900

    2024-01-23 오전

--- java0123/src/swing/MyFrame.java
+++ java0123/src/swing/MyFrame.java
@@ -1,9 +1,27 @@
 package swing;
 import java.awt.Container;
 import java.awt.FlowLayout;
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+class MyEvent implements ActionListener {
+	String str;
+	MyEvent(String str) {
+		this.str = str;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println(str);
+	}
+}
 public class MyFrame extends JFrame {
+//	static String str = "안녕";
+	JTextField tf = new JTextField(10);
+	JButton bt = new JButton("실행");
 	public MyFrame() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container con = this.getContentPane();
@@ -11,6 +29,8 @@ public class MyFrame extends JFrame {
 		this.setLocation(500, 400);
 		this.setSize(400, 300);
 		this.setVisible(true);
+		con.add(tf); con.add(bt);
+		bt.addActionListener(new MyEvent("안녕?"));
 	}
 	
 	public static void main(String[] args) {
