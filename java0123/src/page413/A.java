7bc6df9ed429f6690f1e506998adcd390d6be232
Jiwoong-Jung <jjw981119@naver.com>
2024-01-23 09:27:47 +0900

    2024-01-23 오전

--- java0123/src/page413/A.java
+++ java0123/src/page413/A.java
@@ -2,11 +2,19 @@ package page413;
 
 public class A {
 	int field1;
-	void method1() {}
+	void method1() {
+		System.out.println("method1()");
+	}
 	
 	class B {
 		void method() {
-			
+			field1 = 10;
+			method1();
+			System.out.println("method() ,"+field1);
 		}
+	}
+	
+	public static void main(String[] ar) {
+		System.out.println("실행");
 	}
 }
