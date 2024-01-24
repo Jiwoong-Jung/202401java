36460e87fb848be5141dd08afe03d29fca4bdff9
Jiwoong-Jung <jjw981119@naver.com>
2024-01-24 16:23:55 +0900

    2024-01-24 오후 JDBC 개발

--- java0124/src/jdbc/JdbcEx.java
+++ java0124/src/jdbc/JdbcEx.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class JdbcEx {
 
@@ -11,6 +12,13 @@ public class JdbcEx {
 		String url = "jdbc:mysql://localhost:3306/firm";
 		String id = "root";
 		String pass = "mysql";
+		Scanner scan = new Scanner(System.in);
+		System.out.print("부서번호:");
+		int deptno = Integer.parseInt(scan.nextLine());
+		System.out.print("부서이름:");
+		String dname = scan.nextLine();
+		System.out.print("부서위치:");
+		String loc = scan.nextLine();
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection conn = DriverManager.getConnection(url,id,pass);
 		Statement stmt = conn.createStatement();
