e2e8d2d9c01387b3781bc49a113ddf4f79525cbf
Jiwoong-Jung <jjw981119@naver.com>
2024-01-24 09:44:04 +0900

    2024-01-24 오전

--- java0124/src/sec02/exam01/TryCatchEx.java
+++ java0124/src/sec02/exam01/TryCatchEx.java
@@ -1,5 +1,9 @@
 package sec02.exam01;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TryCatchEx {
 
 	public static void main(String[] args) {
@@ -8,9 +12,17 @@ public class TryCatchEx {
 		} catch(ClassNotFoundException e1) {
 			// 예외 처리 코드
 			System.out.println("클래스가 존재하지 않음!");
+			System.exit(0);
 //			e1.printStackTrace();
 		} finally {
 			System.out.println("반드시 실행");
+		}
+		
+		try {
+			Connection conn = DriverManager.getConnection(null);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 	}
