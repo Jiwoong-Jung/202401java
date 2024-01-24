d096208ef41e84604b3933002ba8210614a1b0ad
Jiwoong-Jung <jjw981119@naver.com>
2024-01-24 14:37:27 +0900

    2024-01-24 오후 익명 객체

--- java0124/src/sec02/exam01/Anonymous.java
+++ java0124/src/sec02/exam01/Anonymous.java
@@ -1,7 +1,20 @@
 package sec02.exam01;
 
 public class Anonymous {
-	Person field;
+	Person field = new Person() {
+		void work() {
+			System.out.println("출근");
+		}
+
+		@Override
+		void wake() {
+			System.out.println("6시 기상");
+			work();
+		}
+		
+	};
+	
+	
 	void method1() {
 		
 	}
