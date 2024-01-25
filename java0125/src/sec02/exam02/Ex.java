72e3c9d9f7b9841eda0fa0bc2994cc0f6a480485
Jiwoong-Jung <jjw981119@naver.com>
2024-01-25 15:15:11 +0900

    2024-01-25 오후 throws

---
+++ java0125/src/sec02/exam02/Ex.java
@@ -0,0 +1,17 @@
+package sec02.exam02;
+
+class AnyClass {
+	void anyMethod() {
+		
+	}
+}
+
+
+public class Ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
