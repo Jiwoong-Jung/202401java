b94d02b1207c71b6362b41e3666983da792d7a8f
Jiwoong-Jung <jjw981119@naver.com>
2024-01-25 11:31:00 +0900

    2024-01-25 오전

---
+++ java0125/src/sec01/exam01/ExceptionEx.java
@@ -0,0 +1,23 @@
+package sec01.exam01;
+
+class MyClass2 {
+	void method() throws MyException {
+		//
+		throw new MyException("나의 예외 상황");
+	}
+}
+
+public class ExceptionEx {
+
+	public static void main(String[] args) {
+		MyClass2 mc = new MyClass2();
+		try {
+			mc.method();
+		} catch (MyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
