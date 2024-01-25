b97acde4a3bb77811444cff43f3049c2a51f348e
Jiwoong-Jung <jjw981119@naver.com>
2024-01-25 10:50:21 +0900

    2024-01-25 오전

--- java0125/src/sec01/exam01/AIOOBEex.java
+++ java0125/src/sec01/exam01/AIOOBEex.java
@@ -3,8 +3,16 @@ package sec01.exam01;
 public class AIOOBEex {
 
 	public static void main(String[] args) {
-		String str = args[0];
-
+		try {
+			String str = args[0];
+			String str1 = args[1];
+			
+			System.out.println(str);
+			System.out.println(str1);
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("예외 발생!");
+		}
+		
 	}
 
 }
