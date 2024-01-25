8572af80c7f4b546a767bb47c7893e7824fd7f6c
Jiwoong-Jung <jjw981119@naver.com>
2024-01-25 15:22:10 +0900

    2024-01-25 오후 throws

--- java0125/src/sec02/exam02/Ex.java
+++ java0125/src/sec02/exam02/Ex.java
@@ -1,17 +1,20 @@
 package sec02.exam02;
 
 class AnyClass {
-	void anyMethod() {
-		
+	void anyMethod() throws Exception {}
+	void method() {
+		try {
+			anyMethod();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
 
-
 public class Ex {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		AnyClass ac = new AnyClass();
+		ac.method();
 	}
-
 }
