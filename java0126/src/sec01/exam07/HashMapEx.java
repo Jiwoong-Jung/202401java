7f028eb38e5ce36845b0a7bf3ebfd080da6b0e8d
Jiwoong-Jung <jjw981119@naver.com>
2024-01-26 11:50:43 +0900

    2024-01-26 오전

---
+++ java0126/src/sec01/exam07/HashMapEx.java
@@ -0,0 +1,17 @@
+package sec01.exam07;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapEx {
+
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<>();
+		map.put("김하나", 85);
+		map.put("이하나", 70);
+		map.put("박하나", 95);
+		System.out.println(map.get("이하나"));
+		System.out.println(map.size());
+	}
+
+}
