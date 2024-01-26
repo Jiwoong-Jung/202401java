e0a84fa107f8d2aa3e3bdce60fe46020128129d3
Jiwoong-Jung <jjw981119@naver.com>
2024-01-26 14:27:05 +0900

    2024-01-26 오후 JPanel

--- java0126/src/swing/WinEmp.java
+++ java0126/src/swing/WinEmp.java
@@ -2,9 +2,11 @@ package swing;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class WinEmp extends JFrame {
 	JButton bt1 = new JButton("버튼1");
@@ -16,11 +18,15 @@ public class WinEmp extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container con = this.getContentPane();
 		con.setLayout(new BorderLayout());
-		con.add(bt1, BorderLayout.EAST);
-		con.add(bt2, BorderLayout.WEST);
-		con.add(bt3, BorderLayout.SOUTH);
-		con.add(bt4, BorderLayout.NORTH);
-		con.add(bt5, BorderLayout.CENTER);
+		JPanel jp1 = new JPanel(new FlowLayout());
+		jp1.add(bt1); jp1.add(bt2); jp1.add(bt3); 
+		jp1.add(bt4); jp1.add(bt5);
+		con.add(jp1, BorderLayout.SOUTH);
+//		con.add(bt1, BorderLayout.EAST);
+//		con.add(bt2, BorderLayout.WEST);
+//		con.add(bt3, BorderLayout.SOUTH);
+//		con.add(bt4, BorderLayout.NORTH);
+//		con.add(bt5, BorderLayout.CENTER);
 		this.setLocation(500, 400);
 		this.setSize(400, 300);
 		this.setVisible(true);
