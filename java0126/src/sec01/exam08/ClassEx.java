2b47a0b18f7d615e24fb1df17c64d8620d048aea
Jiwoong-Jung <jjw981119@naver.com>
2024-01-26 11:26:40 +0900

    2024-01-26 오전

---
+++ java0126/src/sec01/exam08/ClassEx.java
@@ -0,0 +1,21 @@
+package sec01.exam08;
+
+class Car { }
+
+public class ClassEx {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		Class clazz = Car.class;  // 1
+		System.out.println(clazz.getName());
+		System.out.println(clazz.getSimpleName());
+		System.out.println(clazz.getPackageName());
+		
+		// 2
+		Class clazz2 =Class.forName("sec01.exam08.Car");
+		System.out.println(clazz2.getName());
+		System.out.println(clazz2.getSimpleName());
+		System.out.println(clazz2.getPackageName());
+
+	}
+
+}
