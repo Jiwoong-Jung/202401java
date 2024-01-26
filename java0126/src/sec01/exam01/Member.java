3d2934a277c46c2f44a20d500e97170a0cc485e3
Jiwoong-Jung <jjw981119@naver.com>
2024-01-26 09:56:40 +0900

    2024-01-26 오전

--- java0126/src/sec01/exam01/Member.java
+++ java0126/src/sec01/exam01/Member.java
@@ -6,5 +6,18 @@ public class Member {
 	public Member(String id) {
 		this.id = id;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Member) {
+			Member member = (Member) obj;
+			if (id.equals(member.id)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
 	
 }
