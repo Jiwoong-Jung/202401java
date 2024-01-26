f5c309238b5bff52c6458314e4abe67e65b146f4
Jiwoong-Jung <jjw981119@naver.com>
2024-01-26 14:38:12 +0900

    2024-01-26 오후 JTextArea

--- java0126/src/swing/WinEmp.java
+++ java0126/src/swing/WinEmp.java
@@ -7,6 +7,8 @@ import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class WinEmp extends JFrame {
 	JButton bt1 = new JButton("전체 내용");
@@ -14,6 +16,7 @@ public class WinEmp extends JFrame {
 	JButton bt3 = new JButton("이름 검색");
 	JButton bt4 = new JButton("수정");
 	JButton bt5 = new JButton("삭제");
+	JTextArea ta = new JTextArea(10, 40);
 	public WinEmp() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container con = this.getContentPane();
@@ -22,13 +25,12 @@ public class WinEmp extends JFrame {
 		jp1.add(bt1); jp1.add(bt2); jp1.add(bt3); 
 		jp1.add(bt4); jp1.add(bt5);
 		con.add(jp1, BorderLayout.SOUTH);
-//		con.add(bt1, BorderLayout.EAST);
-//		con.add(bt2, BorderLayout.WEST);
-//		con.add(bt3, BorderLayout.SOUTH);
-//		con.add(bt4, BorderLayout.NORTH);
-//		con.add(bt5, BorderLayout.CENTER);
+		JScrollPane scroll = new JScrollPane(ta);
+		JPanel jp2 = new JPanel(new FlowLayout());
+		con.add(jp2, BorderLayout.CENTER);
+		jp2.add(scroll);
 		this.setLocation(500, 400);
-		this.setSize(400, 300);
+		this.setSize(500, 300);
 		this.setVisible(true);
 	}
 	
