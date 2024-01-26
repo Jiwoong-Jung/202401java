19b6e6a6aec7ac16dd8c99640e5b6b770048bf14
Jiwoong-Jung <jjw981119@naver.com>
2024-01-26 09:32:33 +0900

    2024-01-26 오전

---
+++ java0126/src/sec01/exam01/ObjEx.java
@@ -0,0 +1,13 @@
+package sec01.exam01;
+
+public class ObjEx {
+
+	public static void main(String[] args) {
+		Object obj1 = new Object();
+		Object obj2 = new Object();
+
+		boolean result = obj1.equals(obj2);
+		System.out.println(result);
+	}
+
+}
