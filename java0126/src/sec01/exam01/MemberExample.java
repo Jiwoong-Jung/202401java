e5268a185abdb73f503569f33e016179d950fdf5
Jiwoong-Jung <jjw981119@naver.com>
2024-01-26 10:22:25 +0900

    2024-01-26 오전

--- java0126/src/sec01/exam01/MemberExample.java
+++ java0126/src/sec01/exam01/MemberExample.java
@@ -4,8 +4,12 @@ package sec01.exam01;
 public class MemberExample {
 	public static void main(String[] args) {
 		Member obj1 = new Member("blue");
-		Member obj2 = new Member("BLUE");
+		Member obj2 = new Member("blue");
 		Member obj3 = new Member("red");
+		
+		System.out.println(obj1);
+		System.out.println(obj2);
+		System.out.println(obj3);
 		
 		if(obj1.equals(obj2)) {
 			System.out.println("같다.");
