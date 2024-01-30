7ba82286340f930b684eefa6f5d8d3da0fdbedad
Jiwoong-Jung <jjw981119@naver.com>
2024-01-30 14:27:18 +0900

    2024-01-30 오후 java

--- java0130/src/sec01/exam22/BoxingEx.java
+++ java0130/src/sec01/exam22/BoxingEx.java
@@ -3,15 +3,24 @@ package sec01.exam22;
 public class BoxingEx {
 
 	public static void main(String[] args) {
+		Integer num1 = new Integer(3);
+		Integer num2 = new Integer(3);
+		System.out.println(num1 == num2);
+		
 		int x = 100;
 		String str = "가나다";
+		//박싱
 		Integer obj2 = Integer.valueOf("100");
 		Integer obj1 = 300;
 		System.out.println(obj1);
+		//언박싱
 		int y = obj1.intValue();
 		System.out.println(y);
 		int z = x + obj1;
 		System.out.println(z);
+		
+		System.out.println(x == obj2);
+		System.out.println(x != obj1);
 	}
 
 }
