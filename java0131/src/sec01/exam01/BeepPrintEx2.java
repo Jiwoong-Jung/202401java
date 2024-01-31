2251fd74d81c1bc8e21e2d8d7818bdb3817861bd
Jiwoong-Jung <jjw981119@naver.com>
2024-01-31 16:25:44 +0900

    2024-01-31 오후 스레드

--- java0131/src/sec01/exam01/BeepPrintEx2.java
+++ java0131/src/sec01/exam01/BeepPrintEx2.java
@@ -3,14 +3,8 @@ package sec01.exam01;
 public class BeepPrintEx2 {
 
 	public static void main(String[] args) {
-		for (int i=0; i < 5; i++) {
-			System.out.println("beep");
-			try {
-				Thread.sleep(500); // 잠시 멈춤 0.5초
-			} catch (InterruptedException e) {
-
-			}  
-		}
+		Thread th = new Thread(new BeepTask());
+		th.start();
 
 		for (int i=0; i < 5; i++) {
 			System.out.println("띵");
