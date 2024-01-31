64c7d555612f0ca1ff5490e8e3258b23ec0fd6e5
Jiwoong-Jung <jjw981119@naver.com>
2024-01-31 15:26:39 +0900

    2024-01-31 오후 확인문제

---
+++ java0131/src/sec01/verify/exam04/ByteToStringEx.java
@@ -0,0 +1,11 @@
+package sec01.verify.exam04;
+
+public class ByteToStringEx {
+
+	public static void main(String[] args) {
+		byte[] bytes = {73, 32, 108, 111, 118};
+		String str = new String(bytes);
+		System.out.println(str);
+	}
+
+}
--- java0131/src/sec01/verify/exam04/MemberEx.java
+++ java0131/src/sec01/verify/exam04/MemberEx.java
@@ -3,6 +3,9 @@ package sec01.verify.exam04;
 public class MemberEx {
 
 	public static void main(String[] args) {
+		Class clazz = Member.class;
+		System.out.println(clazz.getName());
+		
 		Member member = new Member("blue", "이파란");
 		System.out.println(member);
 
