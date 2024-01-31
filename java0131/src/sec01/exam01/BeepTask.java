7a5dcc460f2c3dd0e172b9f5383bf60359b43e13
Jiwoong-Jung <jjw981119@naver.com>
2024-01-31 16:19:48 +0900

    2024-01-31 오후 스레드

---
+++ java0131/src/sec01/exam01/BeepTask.java
@@ -0,0 +1,18 @@
+package sec01.exam01;
+
+public class BeepTask implements Runnable {
+
+	@Override
+	public void run() {
+		for (int i=0; i < 5; i++) {
+			System.out.println("beep");
+			try {
+				Thread.sleep(500); // 잠시 멈춤 0.5초
+			} catch (InterruptedException e) {
+
+			}  
+		}
+		
+	}
+
+}
