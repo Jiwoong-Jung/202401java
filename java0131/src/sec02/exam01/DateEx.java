493f6ecc6a97bd14d7953ceb1530ffe828a70d3f
Jiwoong-Jung <jjw981119@naver.com>
2024-01-31 15:52:00 +0900

    2024-01-31 오후 날짜 형식

--- java0131/src/sec02/exam01/DateEx.java
+++ java0131/src/sec02/exam01/DateEx.java
@@ -1,6 +1,9 @@
 package sec02.exam01;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Date;
 
 public class DateEx {
@@ -11,6 +14,13 @@ public class DateEx {
 		SimpleDateFormat sdf 
 		    = new SimpleDateFormat("yyyy년MM월dd일 hh시mm분ss초");
 		System.out.println(sdf.format(now));
+		
+		LocalDate date1 = LocalDate.now();
+		System.out.println(date1);
+		LocalTime date2 = LocalTime.now();
+		System.out.println(date2);
+		LocalDateTime date3 = LocalDateTime.now();
+		System.out.println(date3);
 	}
 
 }
