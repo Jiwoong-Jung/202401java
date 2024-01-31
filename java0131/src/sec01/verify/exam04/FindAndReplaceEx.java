1c9a2a04a5ca52d5c17ed5fa65c36a7651b7018c
Jiwoong-Jung <jjw981119@naver.com>
2024-01-31 15:36:23 +0900

    2024-01-31 오후 확인문제

--- java0131/src/sec01/verify/exam04/FindAndReplaceEx.java
+++ java0131/src/sec01/verify/exam04/FindAndReplaceEx.java
@@ -3,10 +3,16 @@ package sec01.verify.exam04;
 public class FindAndReplaceEx {
 
 	public static void main(String[] args) {
+		String strData1 = "200";
+		int intData1 = Integer.parseInt(strData1);
+		int intData2 = 150;
+		String strData2 = String.valueOf(intData2);
+		
+		
 		String str = "모든 프로그램은 자바 언어로";
-		int index = str.indexOf("자바");
+		int index = str.indexOf("자바"); // 위치를 정수로
 		System.out.println(index);
-		str = str.replace("자바", "Java");
+		str = str.replace("자바", "Java");  //새로운 문자열
 		System.out.println(str);
 
 	}
