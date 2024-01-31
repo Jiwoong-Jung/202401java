d54883c632a5d669f733b6c8c5d9be4740a9c411
Jiwoong-Jung <jjw981119@naver.com>
2024-01-31 15:19:30 +0900

    2024-01-31 오후 확인문제

---
+++ java0131/src/sec01/verify/exam04/Member.java
@@ -0,0 +1,13 @@
+package sec01.verify.exam04;
+
+public class Member {
+	private String id;
+	private String name;
+	public Member(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	
+}
---
+++ java0131/src/sec01/verify/exam04/MemberEx.java
@@ -0,0 +1,11 @@
+package sec01.verify.exam04;
+
+public class MemberEx {
+
+	public static void main(String[] args) {
+		Member member = new Member("blue", "이파란");
+		System.out.println(member);
+
+	}
+
+}
